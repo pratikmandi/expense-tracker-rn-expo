eea1a8ab7e07d44704a70b13475f7189a1daacba
pratikmandi <pratikmandi0984@gmail.com>
2025-11-06 20:12:27 +0530

    POST API Created: user can post transaction/expense

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 async function initDB(){
     try {
         await sql `CREATE TABLE IF NOT EXISTS transactions(
@@ -21,6 +23,28 @@ async function initDB(){
         console.log("DB initialization failed", error);
     }
 }
+
+//CREATE TRANSACTION
+app.post("/api/transactions", async (req, res)=>{
+    try {
+        const {user_id, title, category, amount} = req.body;
+        
+        if(!user_id || !title || !category || amount == undefined){
+            return res.status(400).json({message: "All fields are required."})
+        }
+
+        const transaction = await sql`
+        INSERT INTO transactions(user_id,title,category,amount)
+        VALUES(${user_id}, ${title}, ${category}, ${amount})
+        RETURNING *
+        `
+        console.log(transaction)
+        res.status(200).json(transaction[0]);
+    } catch (error) {
+        console.log("Error creating transaction", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+})
 
 app.get("/", (req, res)=>{
     res.send("Hello from server");
