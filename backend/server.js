18308eb160e216f8c79761b3cb8eba7ec3c48ed6
pratikmandi <pratikmandi0984@gmail.com>
2025-11-06 19:42:39 +0530

    Project Structured: NeonDB initializatied express server db transaction table added

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+async function initDB(){
+    try {
+        await sql `CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+        console.log("Database initialized successfully.")
+    } catch (error) {
+        console.log("DB initialization failed", error);
+    }
+}
+
+app.get("/", (req, res)=>{
+    res.send("Hello from server");
+});
+
+initDB().then(()=> {
+    app.listen(process.env.PORT, ()=> {
+    console.log("Server running!");
+})
+})
