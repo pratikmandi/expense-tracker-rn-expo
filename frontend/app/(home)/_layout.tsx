45075d96707bc114d288c02a36b04bb4eb207706
pratikmandi <pratikmandi0984@gmail.com>
2025-11-10 00:37:10 +0530

    frontend: redirect to sign in page if user is not signed in

--- frontend/app/(home)/_layout.tsx
+++ frontend/app/(home)/_layout.tsx
@@ -1,6 +1,14 @@
+import { Redirect } from "expo-router";
 import { Stack } from "expo-router/stack";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function Layout() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
+
+  if (!isSignedIn) return <Redirect href={"/sign-in"} />;
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
