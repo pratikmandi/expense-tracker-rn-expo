a2acaa4564b4e4f147584033798af10347114216
pratikmandi <pratikmandi0984@gmail.com>
2025-11-23 23:53:38 +0530

    backend: fix typo

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -87,7 +87,7 @@ export async function getSummaryByUserId(req, res) {
         res.status(200).json({
             balance: balanceResult[0].balance,
             income: incomeResult[0].income,
-            expense: expenseResult[0].expenses,
+            expenses: expenseResult[0].expenses,
         });
     } catch (error) {
         console.log("Error fetching summary", error);
