f50e47e7d68447176d87b386e65618de22f07ea6
pratikmandi <pratikmandi0984@gmail.com>
2025-11-24 00:20:02 +0530

    validate string user

--- backend/utils/validateUser.js
+++ backend/utils/validateUser.js
@@ -1,17 +1,6 @@
-import { sql } from "../config/db.js";
-
 export async function validateUser(userID, res) {
-    if (isNaN(parseInt(userID))) {
+    if (!userID || typeof userID !== "string") {
         res.status(400).json({ message: "Invalid user ID" });
-        return null;
-    }
-
-    const user = await sql`
-        SELECT user_id FROM transactions WHERE user_id = ${userID}
-    `;
-
-    if (user.length === 0) {
-        res.status(404).json({ message: "User does not exist" });
         return null;
     }
 
